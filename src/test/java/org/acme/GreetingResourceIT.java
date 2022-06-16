930a2f487da034437ade57cb9386b62eaa9fc9a8
alitari <alitari67@gmail.com>
2022-06-16 11:02:46 +0000

    Implement native test

--- src/test/java/org/acme/GreetingResourceIT.java
+++ src/test/java/org/acme/GreetingResourceIT.java
@@ -1,9 +1,20 @@
 package org.acme;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+import org.junit.jupiter.api.Test;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
+
 
 @QuarkusIntegrationTest
 public class GreetingResourceIT extends GreetingResourceTest {
 
-    // Execute the same tests but in native mode.
+    @Test
+    public void testHelloEndpoint() {
+        given()
+          .when().get("/hello")
+          .then()
+             .statusCode(200)
+             .body(is("Hello from RESTEasy Reactive"));
+    }
 }
